7758f5fcda44c76ac3d4aab85e1aaf168da0344d
mayol333 <s.moniczewski@gmail.com>
2024-09-18 19:16:30 +0200

    json server

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect } from "react";
+import styled from "styled-components";
 
-function App() {
-  const [count, setCount] = useState(0)
+const MasterWrapper = styled.div`
+    height: 100px;
+    background-color: red;
+`;
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+export const App = () => {
+    useEffect(() => {
+        fetch("http://localhost:8000/posts")
+            .then((response) => response.json())
+            .then((data) => console.log(data));
+    }, []);
 
-export default App
+    return <MasterWrapper></MasterWrapper>;
+};
